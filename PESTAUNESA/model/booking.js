920769174c3a04f02d7300fc9fb452a74b83cff5
Krize <krisnaayui321@gmail.com>
2022-12-19 01:35:22 +0700

    booking

--- PESTAUNESA/model/booking.js
+++ PESTAUNESA/model/booking.js
@@ -22,6 +22,10 @@ const bookingSchema = new mongoose.Schema(
       type: String,
       require: true,
     },
+    validation: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
